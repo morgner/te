e64e9829def9f4fabd53d88b9575ab80ad1d238e
Manfred Morgner <manfred.morgner@gmail.com>
2018-05-17 23:58:56 +0200

    3 more tests

---
+++ tests/te_tests_m011.cpp
@@ -0,0 +1,58 @@
+#include "cute/cute.h"
+#include "cute/cute_runner.h"
+#include "cute/tap_listener.h"
+
+#include "te.h"
+
+#include <fstream>
+#include <sstream>
+
+template<typename ConcreteTest>
+struct FilesystemTemplateTest
+  {
+  FilesystemTemplateTest()
+    {
+    static_cast<ConcreteTest *>(this)->GenerateTemplates();
+    }
+  };
+
+struct SimpleReplacementTest : FilesystemTemplateTest<SimpleReplacementTest>
+  {
+  static constexpr auto TemplateNameData = "simple_data.tpl";
+  static constexpr auto TemplateNameBase = "simple_base.tpl";
+
+  void GenerateTemplates()
+    {
+    auto outputData = std::ofstream{TemplateNameData};
+    outputData << R"({% extends "simple_base.tpl" %})";
+    auto outputBase = std::ofstream{TemplateNameBase};
+    outputBase << R"({{ title }}:{% block A %} body {% endblock %})";
+    }
+
+  void test_simple_variable_replacement()
+    {
+    auto buffer = std::ostringstream{};
+    TMapS2M mVariables{
+        { "title", { { "", "My Blog" }} }
+    };
+
+    auto oTe = Cte{mVariables, TemplateNameData};
+
+    buffer << oTe;
+
+    ASSERT_EQUAL(buffer.str(), R"(My Blog: body )");
+    }
+  };
+
+int main(int argc, char const * const * argv)
+  {
+  auto listener = cute::tap_listener<cute::null_listener>{};
+  auto runner = cute::makeRunner(listener, argc, argv);
+
+  auto suite = cute::suite{};
+
+  auto oSimpleReplacementTest = SimpleReplacementTest{};
+  suite += CUTE_MEMFUN(oSimpleReplacementTest, SimpleReplacementTest, test_simple_variable_replacement);
+
+  return !runner(suite);
+  }
